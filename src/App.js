7402fbc06222a82a716d7188a4374007d90b70eb
Manuel Hernandez <manuel_alejandro090401@outlook.com>
2022-11-11 13:21:25 -0700

    primeras pruebas

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let auth =false;
+  let nombre = "manuel";
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Editar <code>src/App.js</code> and save to reload.
+
         </p>
+        {/* Pruebas */}
+        <label htmlFor="nombre">Nombre</label>
+        <input type="text" id="nombre" />
+        <p>
+          {auth ? "El usuario: " + nombre +" ha sido logueado" : "No hay una sesion activa"}
+        </p>
+        {/* End pruebas */}
+
+        
         <a
           className="App-link"
           href="https://reactjs.org"
