ccaeeee4d47b827156123493d422697c02dcb2bb
Manuel Alejandro Diaz Hernandez <109549833+Manuhssj@users.noreply.github.com>
2023-05-16 14:23:27 -0700

    Delete src directory

--- src/HelloWorldApp.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-
-// export const HelloWorldApp = () => {
-//     return (
-//         <div>hello world app</div>        
-//     );
-// }
