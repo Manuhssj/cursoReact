0c388cdeefea4e1df77afc150cf55f4b6facafd1
Manuel Hernandez <manuhssj@iMac-de-UABCS.local>
2023-05-29 11:36:15 -0700

    update

--- seccion7/src/components/GifGrid.jsx
+++ seccion7/src/components/GifGrid.jsx
@@ -1,17 +1,11 @@
 
 export const GifGrid = ({ category }) => {
 
-    const gifs = [1,2,3,4,5]
 
     return (
         <>
             <h4> {category} </h4>
             {
-                gifs.map( (gif) => (
-                    
-                    <p>{gif}</p>
-                    
-                ))
 
             }
         </>
