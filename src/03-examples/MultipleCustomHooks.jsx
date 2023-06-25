8008fb8ad9e7d80b06b29bfcef2b05608c9af6f4
Manuel Diaz <mandrodiaz@gmail.com>
2023-06-24 20:48:24 -0700

    update cambios, video 126

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,6 +1,13 @@
+import { useFetch } from '../hooks/useFetch'
 
 export const MultipleCustomHooks = () => {
+
+  const { data, isLoading, hasError } = useFetch('https://api.breakingbadquotes.xyz/v1/quotes')
+  console.log({ data, isLoading, hasError });
   return (
-    <h1>BreakingBad Quotes</h1>
+    <>
+      <h1>BreakingBad Quotes</h1>
+      <hr/>
+    </>
   )
 }
